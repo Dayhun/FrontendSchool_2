2ac3e51931e4d641f3abe0d85ffee2080b4fc9b6
Dayhun <82689971+Dayhun@users.noreply.github.com>
2022-05-03 19:52:59 -0700

    Delete 📚5주차/0428 directory

--- 📚5주차/0428/002.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("one").innerHTML = "hello <strong>world document</strong>";
-console.log("hello world console");
-window.alert("hello world alert");
-document.write("hello!!");
